3527f41e5e34f6820a1ac6b814703cc008298662
PhilipCarmine <philip@wisenet.co>
2018-04-27 14:20:16 +1000

    Add files via upload

---
+++ ExploritoryDataAnalysis/Week1Assignment/Plot1.R
@@ -0,0 +1,15 @@
+dataset <- read.table('C:/Users/carminep/datasciencecoursera/ExploritoryDataAnalysis/Week1Assignment/ProjectData/household_power_consumption.txt', header = TRUE, sep = ";")
+
+dataset2 <- subset(dataset, Date == "1/2/2007")
+dataset3 <- subset(dataset, Date == "2/2/2007")
+dataset4 <- rbind(dataset2, dataset3) 
+
+
+d10 <- as.numeric(as.character(dataset4[,3]))
+
+hist(d10, breaks = 24, main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+
+
+dev.copy(png, file = "C:/Users/carminep/datasciencecoursera/ExploritoryDataAnalysis/Week1Assignment/Plot1.png")
+
+dev.off()
